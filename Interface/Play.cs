eac4a450d1ae87fff4a5da4ccf7ce1bb6cb941a1
OG4rcia <astra10071@gmail.com>
2023-04-01 15:14:02 -0300

    atualização movimentação personagem

---
+++ Interface/Play.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+public partial class Play : Node
+{
+	private void OnPlayButtonPressed()
+	{
+		{
+			GetTree().ChangeSceneToFile("res://Interface/player.tscn");
+		}
+	}
+
+}
+
+
+
